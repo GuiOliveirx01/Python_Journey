fbd65a5e8273dfb8a9d327a5e76fac325cc73d72
GuiOliverx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-03-07 12:22:19 -0300

    file name change

---
+++ teste.py
@@ -0,0 +1,20 @@
+"""n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+result= n1+n2
+print (result)
+print (n1+n2)"""
+
+#n1 = input ("Digite um número:"int)
+"""n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+print (n1+n2)"""
+
+#n1 = int(int==input("Digite um número: "))
+#n2 = int(int==input("Digite um número: "))
+#print (n1+n2)
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite um número: "))
+result = n1 + n2
+result1 = int (result)
+print (result1)
