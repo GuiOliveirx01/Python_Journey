9f82ce4a6191d42e261a54216751c334d82ab4f9
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-03-19 22:24:42 -0300

    smile changes

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,9 @@
 print("Hello Python world!")
 
-#FAÇA VOCÊ MESMO:
-"""1.1 - Tópicos que eu me interessei no site oficial python: Works; 
+
+"""FAÇA VOCÊ MESMO:
+
+1.1 - Tópicos que eu me interessei no site oficial python: Works; 
 Applications; Docs; Education.
 
 1.2 - Caso não escreva o comando 'print' corretamente ou se esqueça de 
@@ -14,17 +16,11 @@ código pode estar com erro
 2. UMA INTELIGÊNCIA ARTIFICIAL
 3. UM SITE"""
 
-"""VARIÁVEL: toda variável armazena um valor - uma informação associada 
-a essa variável."""
-
 message = "Hello Python world!"
 print(message)
 
 message = "Hello Python Crash Course world!"
 print(message)
-
-"""Podemos mudar o valor de uma variável em nosso programa a qualquer
-momento, e Python sempre manterá o controle do valor atual."""
 
 mesage = "Hello Python Crash Course reader!"
 print(mesage)
